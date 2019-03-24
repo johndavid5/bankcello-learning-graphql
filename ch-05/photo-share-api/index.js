72af6ec58246b335dd591709a0e8af3e868e9169
John D. Aynedjian <johndavid5@yahoo.com>
2019-03-24 18:26:59 -0400

    5-3...type resolvers...

--- ch-05/photo-share-api/index.js
+++ ch-05/photo-share-api/index.js
@@ -4,17 +4,30 @@
 const { ApolloServer } = require('apollo-server')
 
 const typeDefs = `
-    type Query {
-        totalPhotos: Int!
+
+    # 1. Add Photo type definition
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
     }
 
+    # 2. Return list Photo's from allPhotos
+    type Query {
+        totalPhotos: Int!
+        allPhotos: [Photo!]!
+    }
+
+    # 3. Return the newly posted photo from mutation
     type Mutation {
-        postPhoto(
-            name: String! 
-            description: String): Boolean!
+        postPhoto(name: String!
+            description: String): Photo!
     }
 `
 
+// 1. A variable we'll increment for unique ids...
+var _id = 0
 // 2. A data type to store our photos in memory...
 var photos = []
 
@@ -23,15 +36,31 @@ const resolvers = {
         totalPhotos: () => {
             console.log(`totalPhotos(): SHEMP: Retoynin' photos.length = ${photos.length}, Moe...`);
             return photos.length
-        }
+        },
+        allPhotos: () => photos
     },
 
     Mutation: {
         postPhoto(parent, args){ 
             console.log(`postPhoto(): SHEMP: parent = `, parent );
-            console.log(`postPhoto(): SHEMP: Pushin' args=${JSON.stringify(args)} onto photos, Moe...`);
-            photos.push(args)
-            return true
+            console.log(`postPhoto(): SHEMP: args = `, args );
+            var newPhoto = {
+                id: _id++,
+                ...args
+            }
+            console.log(`postPhoto(): SHEMP: Pushin' newPhoto =${JSON.stringify(newPhoto)} onto photos, Moe...`);
+            photos.push(newPhoto)
+            // 3. Return the new photo...
+            return newPhoto
+        }
+    },
+
+    // Create trivial resolver for Photo's url field...
+    Photo: { 
+        url: parent => {
+            let url = `http://johnny.com/img/${parent.id}.jpg` 
+            console.log("Photo.url: parent = ", parent, ", returnin' url = ", url ); 
+            return url; 
         }
     }
 }
